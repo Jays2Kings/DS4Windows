a06b1d6e276e187a8b6b623f4f85449523e8222d
Korney Czukowski <carbofos@seznam.cz>
2017-12-19 18:15:50 +0100

    Review code simplify suggestion, reorder private properties

--- DS4Windows/DS4Forms/LanguagePackComboBox.cs
+++ DS4Windows/DS4Forms/LanguagePackComboBox.cs
@@ -14,9 +14,9 @@ namespace DS4Windows.DS4Forms
     public partial class LanguagePackComboBox : UserControl
     {
         private string _invariantCultureText = "No (English UI)";
-        private string _probingPath = "";
         private string _languageAssemblyName = "DS4Windows.resources.dll";
         private TaskCompletionSource<bool> _languageListInitialized = new TaskCompletionSource<bool>();
+        private string _probingPath = "";
 
         [Category("Action")]
         [Description("Fires when the combo box selected index is changed.")]
@@ -51,16 +51,16 @@ namespace DS4Windows.DS4Forms
         [Description("If probing path has been changed in App.config, add the same string here.")]
         public string ProbingPath
         {
-            get { return this._probingPath; }
-            set { this._probingPath = value; }
+            get { return _probingPath; }
+            set { _probingPath = value; }
         }
 
         [Category("Data")]
         [Description("Filter language assembly file names in order to ont include irrelevant assemblies to the combo box.")]
         public string LanguageAssemblyName
         {
-            get { return this._languageAssemblyName; }
-            set { this._languageAssemblyName = value; }
+            get { return _languageAssemblyName; }
+            set { _languageAssemblyName = value; }
         }
 
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
@@ -91,7 +91,7 @@ namespace DS4Windows.DS4Forms
 
             Task.Run(() => {
                 // Find available language assemblies and bind the list to the combo box.
-                cbCulture.DataSource = this.CreateLanguageAssembliesBindingSource();
+                cbCulture.DataSource = CreateLanguageAssembliesBindingSource();
                 cbCulture.SelectedValue = Thread.CurrentThread.CurrentUICulture.Name;
 
                 // This must be set here instead of Designer or event would fire at initial selected value setting above.
@@ -107,7 +107,7 @@ namespace DS4Windows.DS4Forms
         {
             // Find the location where application installed.
             string exeLocation = Path.GetDirectoryName(Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path));
-            List<string> lookupPaths = this.ProbingPath.Split(';')
+            List<string> lookupPaths = ProbingPath.Split(';')
                 .Select(path => Path.Combine(exeLocation, path))
                 .Where(path => path != exeLocation)
                 .ToList();
@@ -115,17 +115,17 @@ namespace DS4Windows.DS4Forms
 
             // Get all culture for which satellite folder found with culture code, then insert invariant culture at the beginning.
             List<KeyValuePair<string, string>> cultures = CultureInfo.GetCultures(CultureTypes.AllCultures)
-                .Where(c => this.IsLanguageAssemblyAvailable(lookupPaths, c))
+                .Where(c => IsLanguageAssemblyAvailable(lookupPaths, c))
                 .Select(c => new KeyValuePair<string, string>(c.Name, c.NativeName))
                 .ToList();
-            cultures.Insert(0, new KeyValuePair<string, string>("", this.InvariantCultureText));
+            cultures.Insert(0, new KeyValuePair<string, string>("", InvariantCultureText));
 
             return new BindingSource(cultures, null);
         }
 
         private bool IsLanguageAssemblyAvailable(List<string> lookupPaths, CultureInfo culture)
         {
-            return lookupPaths.Select(path => Path.Combine(path, culture.Name, this.LanguageAssemblyName))
+            return lookupPaths.Select(path => Path.Combine(path, culture.Name, LanguageAssemblyName))
                 .Where(path => File.Exists(path))
                 .Count() > 0;
         }
@@ -141,17 +141,17 @@ namespace DS4Windows.DS4Forms
         private void LanguagePackComboBox_SizeChanged(object sender, EventArgs e)
         {
             cbCulture.Left = label1.Margin.Left + label1.Width + label1.Margin.Right;
-            cbCulture.Width = this.Width - cbCulture.Left - cbCulture.Margin.Right - cbCulture.Margin.Left;
+            cbCulture.Width = Width - cbCulture.Left - cbCulture.Margin.Right - cbCulture.Margin.Left;
         }
 
         private void CbCulture_SelectedIndexChanged(object sender, EventArgs e)
         {
-            this.SelectedIndexChanged?.Invoke(this, e);
+            SelectedIndexChanged?.Invoke(this, e);
         }
 
         private void CbCulture_SelectedValueChanged(object sender, EventArgs e)
         {
-            this.SelectedValueChanged?.Invoke(this, e);
+            SelectedValueChanged?.Invoke(this, e);
         }
     }
 }

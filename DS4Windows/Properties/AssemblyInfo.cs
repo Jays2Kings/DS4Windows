164f6f65e39b23a01cd3b9b587b0aed39312199b
Travis Nickles <nickles.travis@gmail.com>
2018-02-22 17:57:37 -0600

    Version 1.4.112

--- DS4Windows/Properties/AssemblyInfo.cs
+++ DS4Windows/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.111")]
-[assembly: AssemblyFileVersion("1.4.111")]
+[assembly: AssemblyVersion("1.4.112")]
+[assembly: AssemblyFileVersion("1.4.112")]
 [assembly: NeutralResourcesLanguage("en")]
 

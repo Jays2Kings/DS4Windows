605c700362dc6c16643e1e183b99fd3f45c630f8
Travis Nickles <nickles.travis@gmail.com>
2018-07-17 20:45:20 -0500

    Exit GetPadDetailForIdx after processing null reference
    Might be related to issue #326.

--- DS4Windows/DS4Control/ControlService.cs
+++ DS4Windows/DS4Control/ControlService.cs
@@ -64,7 +64,7 @@ namespace DS4Windows
                 meta.Model = DsModel.None;
                 meta.BatteryStatus = 0;
                 meta.IsActive = false;
-
+                return;
                 //return meta;
             }
 

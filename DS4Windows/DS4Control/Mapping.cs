8783f199aa0da9f4920b1fb9cdaea21c0a862e07
Travis Nickles <nickles.travis@gmail.com>
2018-12-11 02:27:57 -0600

    Revert "Lowered enhanced precision curve"
    This reverts commit a97af009485dad924cf25a8232d111ffd0da16e4.

--- DS4Windows/DS4Control/Mapping.cs
+++ DS4Windows/DS4Control/Mapping.cs
@@ -779,28 +779,28 @@ namespace DS4Windows
 
                     if (absX <= 0.4)
                     {
-                        outputX = 0.489 * absX;
+                        outputX = 0.49 * absX;
                     }
                     else if (absX <= 0.75)
                     {
-                        outputX = absX - 0.2044;
+                        outputX = absX - 0.204;
                     }
                     else if (absX > 0.75)
                     {
-                        outputX = (absX * 1.8176) - 0.8176;
+                        outputX = (absX * 1.816) - 0.816;
                     }
 
                     if (absY <= 0.4)
                     {
-                        outputY = 0.489 * absY;
+                        outputY = 0.49 * absY;
                     }
                     else if (absY <= 0.75)
                     {
-                        outputY = absY - 0.2044;
+                        outputY = absY - 0.204;
                     }
                     else if (absY > 0.75)
                     {
-                        outputY = (absY * 1.8176) - 0.8176;
+                        outputY = (absY * 1.816) - 0.816;
                     }
 
                     dState.LX = (byte)(outputX * signX * 127.5 + 127.5);
@@ -839,28 +839,28 @@ namespace DS4Windows
 
                     if (absX <= 0.4)
                     {
-                        outputX = 0.489 * absX;
+                        outputX = 0.49 * absX;
                     }
                     else if (absX <= 0.75)
                     {
-                        outputX = absX - 0.2044;
+                        outputX = absX - 0.204;
                     }
                     else if (absX > 0.75)
                     {
-                        outputX = (absX * 1.8176) - 0.8176;
+                        outputX = (absX * 1.816) - 0.816;
                     }
 
                     if (absY <= 0.4)
                     {
-                        outputY = 0.489 * absY;
+                        outputY = 0.49 * absY;
                     }
                     else if (absY <= 0.75)
                     {
-                        outputY = absY - 0.2044;
+                        outputY = absY - 0.204;
                     }
                     else if (absY > 0.75)
                     {
-                        outputY = (absY * 1.8176) - 0.8176;
+                        outputY = (absY * 1.816) - 0.816;
                     }
 
                     dState.RX = (byte)(outputX * signX * 127.5 + 127.5);

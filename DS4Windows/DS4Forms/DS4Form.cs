7818e88b72cfd94de053b28e5bf3d621918e794f
Travis Nickles <nickles.travis@gmail.com>
2019-02-08 11:26:42 -0600

    Added dispose call when closing Options form

--- DS4Windows/DS4Forms/DS4Form.cs
+++ DS4Windows/DS4Forms/DS4Form.cs
@@ -1612,6 +1612,7 @@ Properties.Resources.DS4Update, MessageBoxButtons.YesNo, MessageBoxIcon.Question
 
             opt.Dock = DockStyle.None;
             tabProfiles.Controls.Remove(opt);
+            opt.Dispose();
             optPop = false; opt = null;
 
             lBProfiles.Visible = true;

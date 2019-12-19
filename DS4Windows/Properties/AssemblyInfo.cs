e6a22859a55b4a0cdfdfa99fe07d8a07a22528f9
Travis Nickles <ryochan7@protonmail.com>
2019-12-19 13:26:44 -0600

    Version 1.7.28

--- DS4Windows/Properties/AssemblyInfo.cs
+++ DS4Windows/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.27")]
-[assembly: AssemblyFileVersion("1.7.27")]
+[assembly: AssemblyVersion("1.7.28")]
+[assembly: AssemblyFileVersion("1.7.28")]
 [assembly: NeutralResourcesLanguage("en")]
 

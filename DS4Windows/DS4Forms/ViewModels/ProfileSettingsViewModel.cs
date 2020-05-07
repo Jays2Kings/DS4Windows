12616a5f7e7a5b48e645114a7997415e979f5bab
Travis Nickles <ryochan7@protonmail.com>
2020-05-07 14:10:19 -0500

    Show minimum mouse speed in GUI

--- DS4Windows/DS4Forms/ViewModels/ProfileSettingsViewModel.cs
+++ DS4Windows/DS4Forms/ViewModels/ProfileSettingsViewModel.cs
@@ -445,6 +445,11 @@ namespace DS4WinWPF.DS4Forms.ViewModels
         }
         public event EventHandler ButtonMouseSensitivityChanged;
 
+        private double RawButtonMouseOffset
+        {
+            get => Global.ButtonMouseInfos[device].mouseVelocityOffset;
+        }
+
         public double ButtonMouseOffset
         {
             get => Global.ButtonMouseInfos[device].mouseVelocityOffset * 100.0;
@@ -453,8 +458,10 @@ namespace DS4WinWPF.DS4Forms.ViewModels
                 double temp = Global.ButtonMouseInfos[device].mouseVelocityOffset * 100.0;
                 if (temp == value) return;
                 Global.ButtonMouseInfos[device].mouseVelocityOffset = value * 0.01;
+                ButtonMouseOffsetChanged?.Invoke(this, EventArgs.Empty);
             }
         }
+        public event EventHandler ButtonMouseOffsetChanged;
 
         private int outputMouseSpeed;
         public int OutputMouseSpeed
@@ -468,6 +475,19 @@ namespace DS4WinWPF.DS4Forms.ViewModels
             }
         }
         public event EventHandler OutputMouseSpeedChanged;
+
+        private double mouseOffsetSpeed;
+        public double MouseOffsetSpeed
+        {
+            get => mouseOffsetSpeed;
+            set
+            {
+                if (mouseOffsetSpeed == value) return;
+                mouseOffsetSpeed = value;
+                MouseOffsetSpeedChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        public event EventHandler MouseOffsetSpeedChanged;
 
         public bool MouseAcceleration
         {
@@ -1541,6 +1561,7 @@ namespace DS4WinWPF.DS4Forms.ViewModels
             tempBtPollRate = Global.BTPollRate[device];
 
             outputMouseSpeed = CalculateOutputMouseSpeed(ButtonMouseSensitivity);
+            mouseOffsetSpeed = RawButtonMouseOffset * outputMouseSpeed;
 
             ImageSourceConverter sourceConverter = new ImageSourceConverter();
             ImageSource temp = sourceConverter.
@@ -1549,6 +1570,16 @@ namespace DS4WinWPF.DS4Forms.ViewModels
 
             presetMenuUtil = new PresetMenuHelper(device);
 
+            SetupEvents();
+        }
+
+        private void CalcProfileFlags(object sender, EventArgs e)
+        {
+            Global.cacheProfileCustomsFlags(device);
+        }
+
+        private void SetupEvents()
+        {
             MainColorChanged += ProfileSettingsViewModel_MainColorChanged;
             MainColorRChanged += (sender, args) =>
             {
@@ -1577,6 +1608,7 @@ namespace DS4WinWPF.DS4Forms.ViewModels
             ButtonMouseSensitivityChanged += (sender, args) =>
             {
                 OutputMouseSpeed = CalculateOutputMouseSpeed(ButtonMouseSensitivity);
+                MouseOffsetSpeed = RawButtonMouseOffset * OutputMouseSpeed;
             };
 
             LightbarModeIndexChanged += (sender, args) =>
@@ -1586,11 +1618,13 @@ namespace DS4WinWPF.DS4Forms.ViewModels
 
             GyroOutModeIndexChanged += CalcProfileFlags;
             SASteeringWheelEmulationAxisIndexChanged += CalcProfileFlags;
+            ButtonMouseOffsetChanged += ProfileSettingsViewModel_ButtonMouseOffsetChanged;
         }
 
-        private void CalcProfileFlags(object sender, EventArgs e)
+        private void ProfileSettingsViewModel_ButtonMouseOffsetChanged(object sender,
+            EventArgs e)
         {
-            Global.cacheProfileCustomsFlags(device);
+            MouseOffsetSpeed = RawButtonMouseOffset * OutputMouseSpeed;
         }
 
         private void ProfileSettingsViewModel_MainColorChanged(object sender, EventArgs e)
@@ -1958,6 +1992,7 @@ namespace DS4WinWPF.DS4Forms.ViewModels
             Global.outDevTypeTemp[device] = Global.OutContType[device];
             tempBtPollRate = Global.BTPollRate[device];
             outputMouseSpeed = CalculateOutputMouseSpeed(ButtonMouseSensitivity);
+            mouseOffsetSpeed = RawButtonMouseOffset * outputMouseSpeed;
         }
     }
 

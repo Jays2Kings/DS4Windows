6f5b8391f47551baacee38f9d9a1749d327fdebd
Travis Nickles <ryochan7@protonmail.com>
2020-08-26 15:30:05 -0500

    Make converter to escape underscores. Remove access key usage for some controls
    Related to issue #1457

---
+++ DS4Windows/DS4Forms/Converters/EscapeAccessKeysConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DS4WinWPF.DS4Forms.Converters
+{
+    public class EscapeAccessKeysConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string temp = value.ToString();
+            temp = Regex.Replace(temp, "_{1}", "__");
+            return temp;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}

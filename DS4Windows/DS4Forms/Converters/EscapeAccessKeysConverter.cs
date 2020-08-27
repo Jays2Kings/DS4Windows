d8d465cd260a58742c63a6d31689a2e3336a33aa
Travis Nickles <ryochan7@protonmail.com>
2020-08-26 22:03:53 -0500

    Implemented a ConvertBack routine for the EscapeAccessKeysConverter
    Don't plan on using it though

--- DS4Windows/DS4Forms/Converters/EscapeAccessKeysConverter.cs
+++ DS4Windows/DS4Forms/Converters/EscapeAccessKeysConverter.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace DS4WinWPF.DS4Forms.Converters
@@ -20,7 +16,9 @@ namespace DS4WinWPF.DS4Forms.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value;
+            string temp = value.ToString();
+            temp = Regex.Replace(temp, "_{2}", "_");
+            return temp;
         }
     }
 }

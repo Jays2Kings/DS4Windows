97d01364eb90d9de907ec1a2fd4d109f6a64627d
Travis Nickles <ryochan7@protonmail.com>
2020-11-30 00:19:31 -0600

    Use custom window class for popup Log messages. Can display clickable hyperlink text

---
+++ DS4Windows/DS4Forms/LogMessageDisplay.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using MarkdownEngine = Markdown.Xaml.Markdown;
+
+namespace DS4WinWPF.DS4Forms
+{
+    /// <summary>
+    /// Interaction logic for LogMessageDisplay.xaml
+    /// </summary>
+    public partial class LogMessageDisplay : Window
+    {
+        public LogMessageDisplay()
+        {
+            InitializeComponent();
+        }
+
+        public LogMessageDisplay(string message) : this()
+        {
+            Regex urlReg = new Regex(@"http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?");
+            message = urlReg.Replace(message, "[$0]($0)");
+
+            MarkdownEngine engine = new MarkdownEngine();
+            FlowDocument tmpDoc = engine.Transform(message);
+            tmpDoc.TextAlignment = TextAlignment.Center;
+
+            richMessageBox.CommandBindings.Add(new CommandBinding(
+                NavigationCommands.GoToPage,
+                (sender, e) =>
+                {
+                    Process proc = new Process();
+                    proc.StartInfo.UseShellExecute = true;
+                    proc.StartInfo.FileName = (string)e.Parameter;
+
+                    proc.Start();
+                }));
+
+            richMessageBox.Document = tmpDoc;
+        }
+
+        private void OkayButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}

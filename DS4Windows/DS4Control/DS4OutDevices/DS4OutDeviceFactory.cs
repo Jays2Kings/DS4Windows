efe5d19d19c599735342ac6af7bcb904a5781128
Travis Nickles <ryochan7@protonmail.com>
2020-12-24 10:38:06 -0600

    Fixed version compare clause when checking ViGEmBus version number
    Make sure to use old API class as needed

--- DS4Windows/DS4Control/DS4OutDevices/DS4OutDeviceFactory.cs
+++ DS4Windows/DS4Control/DS4OutDevices/DS4OutDeviceFactory.cs
@@ -15,7 +15,7 @@ namespace DS4Windows
             Version driverVersion)
         {
             DS4OutDevice result = null;
-            if (extAPIMinVersion.CompareTo(driverVersion) >= 0)
+            if (extAPIMinVersion.CompareTo(driverVersion) <= 0)
             {
                 result = new DS4OutDeviceExt(client);
             }

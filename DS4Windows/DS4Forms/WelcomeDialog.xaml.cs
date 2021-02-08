190e21e24add1fe0e1bf87c07eab2f360904051a
Travis Nickles <ryochan7@protonmail.com>
2021-02-08 13:04:24 -0600

    Disable Xbox 360 installer button in Welcome Dialog if running on Win8 or greater
    Driver comes pre-installed on the OS

--- DS4Windows/DS4Forms/WelcomeDialog.xaml.cs
+++ DS4Windows/DS4Forms/WelcomeDialog.xaml.cs
@@ -54,6 +54,13 @@ namespace DS4WinWPF.DS4Forms
             }
 
             tempInstFileName = DS4Windows.Global.exedirpath + $"\\{installFileName}.tmp";
+
+            // Disable Xbox 360 driver installer button if running on Windows 8 or greater.
+            // Driver come pre-installed on a standed OS install
+            if (DS4Windows.Global.IsWin8OrGreater())
+            {
+                step2Btn.IsEnabled = false;
+            }
         }
 
         private void FinishedBtn_Click(object sender, RoutedEventArgs e)

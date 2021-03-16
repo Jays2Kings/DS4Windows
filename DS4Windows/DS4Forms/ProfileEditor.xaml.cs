4e7c3b32c8ae39de1f80f5121c7c03f0e21ed611
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2021-03-16 14:43:34 -0500

    Fix ColorByBattery panel visibility for first open
    No profile data was loaded in the constructor, so this method will always hide the ColorByBattery panel.

--- DS4Windows/DS4Forms/ProfileEditor.xaml.cs
+++ DS4Windows/DS4Forms/ProfileEditor.xaml.cs
@@ -76,7 +76,6 @@ namespace DS4WinWPF.DS4Forms
             PopulateHoverIndexes();
             PopulateReverseHoverIndexes();
 
-            ColorByBatteryPerCheck();
             AssignTiltAssociation();
             AssignSwipeAssociation();
             AssignTriggerFullPullAssociation();
@@ -547,6 +546,8 @@ namespace DS4WinWPF.DS4Forms
                     Global.LoadBlankDevProfile(device, false, App.rootHub, false);
                 }
             }
+
+            ColorByBatteryPerCheck();
 
             if (device < Global.TEST_PROFILE_INDEX)
             {

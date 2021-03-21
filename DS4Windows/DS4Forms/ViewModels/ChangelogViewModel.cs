d75cecf934af9a12d94559f12081086561ade511
Travis Nickles <ryochan7@protonmail.com>
2021-03-21 12:04:28 -0500

    Removed Newtonsoft.Json library dependency
    Now use System.Text.Json classes included
    in .NET 5

--- DS4Windows/DS4Forms/ViewModels/ChangelogViewModel.cs
+++ DS4Windows/DS4Forms/ViewModels/ChangelogViewModel.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Documents;
 using HttpProgress;
-using Newtonsoft.Json;
+using System.Text.Json;
 using MarkdownEngine = MdXaml.Markdown;
 
 namespace DS4WinWPF.DS4Forms.ViewModels
@@ -61,10 +61,15 @@ namespace DS4WinWPF.DS4Forms.ViewModels
                 string temp = File.ReadAllText(filename).Trim();
                 try
                 {
-                    ChangelogInfo tempInfo = JsonConvert.DeserializeObject<ChangelogInfo>(temp);
+                    JsonSerializerOptions options = new JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = true,
+                    };
+                    options.Converters.Add(new DateTimeJsonConverter.DateTimeConverterUsingDateTimeParse());
+                    ChangelogInfo tempInfo = JsonSerializer.Deserialize<ChangelogInfo>(temp, options);
                     BuildChangelogDocument(tempInfo);
                 }
-                catch (JsonSerializationException) { }
+                catch (JsonException) {}
             }
             else if (!readFile)
             {

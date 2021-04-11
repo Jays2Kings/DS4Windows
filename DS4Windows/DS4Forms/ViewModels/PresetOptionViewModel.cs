b40f7be339564b919a7d12441cc2b21bebf3da44
Travis Nickles <ryochan7@protonmail.com>
2021-04-11 14:46:16 -0500

    Moved EnumChoiceSelection class to its own class file

--- DS4Windows/DS4Forms/ViewModels/PresetOptionViewModel.cs
+++ DS4Windows/DS4Forms/ViewModels/PresetOptionViewModel.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DS4WinWPF.DS4Control;
+using DS4WinWPF.DS4Forms.ViewModels.Util;
 
 namespace DS4WinWPF.DS4Forms.ViewModels
 {
---
+++ DS4Windows/DS4Forms/ViewModels/Util/EnumChoiceSelection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DS4WinWPF.DS4Forms.ViewModels.Util
+{
+    public class EnumChoiceSelection<T>
+    {
+        private string displayName;
+        public string DisplayName { get => displayName; }
+
+        private T choiceValue;
+        public T ChoiceValue
+        {
+            get => choiceValue;
+            set => choiceValue = value;
+        }
+
+        public EnumChoiceSelection(string name, T currentValue)
+        {
+            displayName = name;
+            choiceValue = currentValue;
+        }
+    }
+}

361717eb2d0bc2d349e37bbacd87d4e9b95f77e7
mika-n <35538525+mika-n@users.noreply.github.com>
2021-04-17 16:38:08 +0300

    tweaked DS3 device name to match the new mode name in the upcoming version of DsHidMini driver

--- DS4Windows/DS4Library/DS4Devices.cs
+++ DS4Windows/DS4Library/DS4Devices.cs
@@ -158,7 +158,7 @@ namespace DS4Windows
             new VidPidInfo(NINTENDO_VENDOR_ID, JOYCON_L_PRODUCT_ID, "JoyCon (L)", InputDeviceType.JoyConL, VidPidFeatureSet.DefaultDS4, checkConnection: JoyConDevice.DetermineConnectionType),
             new VidPidInfo(NINTENDO_VENDOR_ID, JOYCON_R_PRODUCT_ID, "JoyCon (R)", InputDeviceType.JoyConR, VidPidFeatureSet.DefaultDS4, checkConnection: JoyConDevice.DetermineConnectionType),
             new VidPidInfo(0x7545, 0x1122, "Gioteck VX4", InputDeviceType.DS4), // Gioteck VX4 (no real lightbar, only some RGB leds)
-            new VidPidInfo(0x7331, 0x0001, "DualShock 3 (DS4 mode)", InputDeviceType.DS4, VidPidFeatureSet.NoGyroCalib | VidPidFeatureSet.VendorDefinedDevice), // Sony DualShock 3 using DsHidMini driver both in DS4 mode and as vendor-defined HID device
+            new VidPidInfo(0x7331, 0x0001, "DualShock 3 (DS4 Emulation)", InputDeviceType.DS4, VidPidFeatureSet.NoGyroCalib | VidPidFeatureSet.VendorDefinedDevice), // Sony DualShock 3 using DsHidMini driver. DsHidMini uses vendor-defined HID device type when it's emulating DS3 using DS4 button layout
         };
 
         public static string devicePathToInstanceId(string devicePath)

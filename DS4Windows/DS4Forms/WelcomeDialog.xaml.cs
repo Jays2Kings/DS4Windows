3ce02e8e04b1551f12b770a90e219dd7a7e6a77e
Travis Nickles <ryochan7@protonmail.com>
2021-05-29 13:38:25 -0500

    Add placeholder panel for FakerInput installer link. Some minor tweaks for changing control access

--- DS4Windows/DS4Forms/WelcomeDialog.xaml.cs
+++ DS4Windows/DS4Forms/WelcomeDialog.xaml.cs
@@ -21,6 +21,8 @@ namespace DS4WinWPF.DS4Forms
             "https://github.com/ViGEm/ViGEmBus/releases/download/setup-v1.17.333/ViGEmBusSetup_x86.msi";
 
         private const string InstallerHidHideX64 = "https://github.com/ViGEm/HidHide/releases/download/v1.0.30.0/HidHideMSI.msi";
+        private const string InstallerFakerInputX64 = "https://github.com/Ryochan7/FakerInputSetup/releases/download/v0.0.1/FakerInput_0.0.1_x64.msi";
+        private const string InstallerFakerInputX86 = "https://github.com/Ryochan7/FakerInputSetup/releases/download/v0.0.1/FakerInput_0.0.1_x86.msi";
 
         private const string InstFileName1_16 = "ViGEmBus_Setup_1.16.116.exe";
         private const string InstFileNameX64 = "ViGEmBusSetup_x64.msi";
@@ -28,6 +30,9 @@ namespace DS4WinWPF.DS4Forms
         private string tempInstFileName;
 
         private const string InstHidHideFileNameX64 = "HidHideMSI.msi";
+
+        private string installFakerInputDL = "";
+        private string instFakerInputFileName = "";
 
         // Default to latest known ViGEmBus installer
         private string installDL = InstallerDLX64;
@@ -47,6 +52,7 @@ namespace DS4WinWPF.DS4Forms
 
             InitializeComponent();
 
+            // Run checks for compatible version of ViGEmBus
             if (!DS4Windows.Global.IsWin10OrGreater())
             {
                 installDL = InstallerDL1_16;
@@ -56,6 +62,28 @@ namespace DS4WinWPF.DS4Forms
             {
                 installDL = InstallerDLX86;
                 installFileName = InstFileNameX86;
+            }
+
+            // Run checks for FakerInput driver
+            if (DS4Windows.Global.IsWin8OrGreater())
+            {
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    installFakerInputDL = InstallerFakerInputX64;
+                    instFakerInputFileName = new FileInfo(InstallerFakerInputX64).Name;
+                }
+                else if (!Environment.Is64BitOperatingSystem)
+                {
+                    installDL = InstallerDLX86;
+                    installFileName = InstFileNameX86;
+
+                    installFakerInputDL = InstallerFakerInputX86;
+                    instFakerInputFileName = new FileInfo(InstallerFakerInputX86).Name;
+                }
+            }
+            else
+            {
+                step5FakerInputPanel.IsEnabled = false;
             }
 
             tempInstFileName = DS4Windows.Global.exedirpath + $"\\{installFileName}.tmp";
@@ -72,6 +100,10 @@ namespace DS4WinWPF.DS4Forms
             {
                 step4HidHidePanel.IsEnabled = false;
             }
+
+            // Just leave panel disabled for now. Download link does not
+            // exist currently
+            step5FakerInputPanel.IsEnabled = false;
         }
 
         private bool IsHidHideControlCompatible()
@@ -79,6 +111,13 @@ namespace DS4WinWPF.DS4Forms
             // HidHide only works on Windows 10 x64
             return DS4Windows.Global.IsWin10OrGreater() &&
                 Environment.Is64BitOperatingSystem;
+        }
+
+        private bool IsFakerInputControlCompatible()
+        {
+            // FakerInput works on Windows 8.1 and later. Going to attempt
+            // to support x64 and x86 arch. Just leave disabled for now
+            return false && DS4Windows.Global.IsWin8OrGreater();
         }
 
         private void FinishedBtn_Click(object sender, RoutedEventArgs e)
@@ -98,8 +137,7 @@ namespace DS4WinWPF.DS4Forms
                 File.Delete(tempInstFileName);
             }
 
-            vigemInstallBtn.IsEnabled = false;
-            step4HidHidePanel.IsEnabled = false;
+            EnableControls(false);
             ViGEmDownloadLaunch();
 
             /*WebClient wb = new WebClient();
@@ -149,7 +187,7 @@ namespace DS4WinWPF.DS4Forms
 
             if (success)
             {
-                vigemInstallBtn.IsEnabled = false;
+                EnableControls(false);
 
                 monitorTimer = new NonFormTimer();
                 monitorTimer.Elapsed += ViGEmInstallTimer_Tick;
@@ -158,8 +196,7 @@ namespace DS4WinWPF.DS4Forms
             else
             {
                 vigemInstallBtn.Content = Properties.Resources.InstallFailed;
-                vigemInstallBtn.IsEnabled = true;
-                step4HidHidePanel.IsEnabled = IsHidHideControlCompatible();
+                EnableControls(true);
             }
         }
 
@@ -176,8 +213,7 @@ namespace DS4WinWPF.DS4Forms
                     Dispatcher.BeginInvoke((Action)(() =>
                     {
                         vigemInstallBtn.Content = Properties.Resources.InstallComplete;
-                        vigemInstallBtn.IsEnabled = true;
-                        step4HidHidePanel.IsEnabled = IsHidHideControlCompatible();
+                        EnableControls(true);
                     }));
                 }
                 else
@@ -185,8 +221,7 @@ namespace DS4WinWPF.DS4Forms
                     Dispatcher.BeginInvoke((Action)(() =>
                     {
                         vigemInstallBtn.Content = Properties.Resources.InstallFailed;
-                        vigemInstallBtn.IsEnabled = true;
-                        step4HidHidePanel.IsEnabled = IsHidHideControlCompatible();
+                        EnableControls(true);
                     }), null);
                 }
 
@@ -224,8 +259,7 @@ namespace DS4WinWPF.DS4Forms
                 File.Delete(tempInstHidFileName);
             }
 
-            vigemInstallBtn.IsEnabled = false;
-            step4HidHidePanel.IsEnabled = false;
+            EnableControls(false);
             HidHideDownloadLaunch();
         }
 
@@ -269,8 +303,6 @@ namespace DS4WinWPF.DS4Forms
 
             if (success)
             {
-                hidHideInstallBtn.IsEnabled = false;
-
                 monitorTimer = new NonFormTimer();
                 monitorTimer.Elapsed += HidHideInstallTimer_Elapsed;
                 monitorTimer.Start();
@@ -278,8 +310,7 @@ namespace DS4WinWPF.DS4Forms
             else
             {
                 hidHideInstallBtn.Content = Properties.Resources.InstallFailed;
-                step4HidHidePanel.IsEnabled = IsHidHideControlCompatible();
-                vigemInstallBtn.IsEnabled = true;
+                EnableControls(true);
             }
         }
 
@@ -295,8 +326,7 @@ namespace DS4WinWPF.DS4Forms
                     Dispatcher.BeginInvoke((Action)(() =>
                     {
                         hidHideInstallBtn.Content = Properties.Resources.InstallComplete;
-                        step4HidHidePanel.IsEnabled = true;
-                        vigemInstallBtn.IsEnabled = true;
+                        EnableControls(true);
                     }));
                 }
                 else
@@ -304,8 +334,7 @@ namespace DS4WinWPF.DS4Forms
                     Dispatcher.BeginInvoke((Action)(() =>
                     {
                         hidHideInstallBtn.Content = Properties.Resources.InstallFailed;
-                        step4HidHidePanel.IsEnabled = true;
-                        vigemInstallBtn.IsEnabled = true;
+                        EnableControls(true);
                     }), null);
                 }
 
@@ -318,6 +347,133 @@ namespace DS4WinWPF.DS4Forms
             {
                 ((NonFormTimer)sender).Start();
             }
+        }
+
+        private void FakerInputInstallBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(DS4Windows.Global.exedirpath + $"\\{instFakerInputFileName}"))
+            {
+                File.Delete(DS4Windows.Global.exedirpath + $"\\{instFakerInputFileName}");
+            }
+
+            string tempInstFakerInputName = DS4Windows.Global.exedirpath + $"\\{instFakerInputFileName}.tmp";
+            if (File.Exists(tempInstFakerInputName))
+            {
+                File.Delete(tempInstFakerInputName);
+            }
+
+            EnableControls(false);
+            FakerInputDownloadLaunch();
+        }
+
+        private async void FakerInputDownloadLaunch()
+        {
+            Progress<ICopyProgress> progress = new Progress<ICopyProgress>(x => // Please see "Notes on IProgress<T>"
+            {
+                // This is your progress event!
+                // It will fire on every buffer fill so don't do anything expensive.
+                // Writing to the console IS expensive, so don't do the following in practice...
+                fakerInputInstallBtn.Content = Properties.Resources.Downloading.Replace("*number*%",
+                    x.PercentComplete.ToString("P"));
+                //Console.WriteLine(x.PercentComplete.ToString("P"));
+            });
+
+            string tempInstFakerInputFileName = DS4Windows.Global.exedirpath + $"\\{instFakerInputFileName}.tmp";
+            string filename = DS4Windows.Global.exedirpath + $"\\{instFakerInputFileName}";
+            bool success = false;
+            using (var downloadStream = new FileStream(tempInstFakerInputFileName, FileMode.CreateNew))
+            {
+                HttpResponseMessage response = await App.requestClient.GetAsync(installFakerInputDL,
+                    downloadStream, progress);
+                success = response.IsSuccessStatusCode;
+            }
+
+            if (success)
+            {
+                File.Move(tempInstFakerInputFileName, filename);
+            }
+            success = false; // Reset for later check
+
+            if (File.Exists(DS4Windows.Global.exedirpath + $"\\{instFakerInputFileName}"))
+            {
+                //vigemInstallBtn.Content = Properties.Resources.OpeningInstaller;
+                ProcessStartInfo startInfo = new ProcessStartInfo(DS4Windows.Global.exedirpath + $"\\{instFakerInputFileName}");
+                startInfo.UseShellExecute = true; // Needed to run program as admin
+                monitorProc = Process.Start(startInfo);
+                fakerInputInstallBtn.Content = Properties.Resources.Installing;
+                success = true;
+            }
+
+            if (success)
+            {
+                monitorTimer = new NonFormTimer();
+                monitorTimer.Elapsed += FakerInputInstallTimer_Elapsed;
+                monitorTimer.Start();
+            }
+            else
+            {
+                fakerInputInstallBtn.Content = Properties.Resources.InstallFailed;
+                EnableControls(true);
+            }
+        }
+
+        private void FakerInputInstallTimer_Elapsed(object sender,
+            System.Timers.ElapsedEventArgs e)
+        {
+            ((NonFormTimer)sender).Stop();
+            bool finished = false;
+            if (monitorProc != null && monitorProc.HasExited)
+            {
+                if (DS4Windows.Global.IsFakerInputInstalled())
+                {
+                    Dispatcher.BeginInvoke((Action)(() =>
+                    {
+                        fakerInputInstallBtn.Content = Properties.Resources.InstallComplete;
+                        EnableControls(true);
+                    }));
+                }
+                else
+                {
+                    Dispatcher.BeginInvoke((Action)(() =>
+                    {
+                        fakerInputInstallBtn.Content = Properties.Resources.InstallFailed;
+                        EnableControls(true);
+                    }), null);
+                }
+
+                File.Delete(DS4Windows.Global.exedirpath + $"\\{instFakerInputFileName}");
+                ((NonFormTimer)sender).Stop();
+                finished = true;
+            }
+
+            if (!finished)
+            {
+                ((NonFormTimer)sender).Start();
+            }
+        }
+
+        private void EnableControls(bool on)
+        {
+            vigemInstallBtn.IsEnabled = on;
+            step4HidHidePanel.IsEnabled = on;
+            step5FakerInputPanel.IsEnabled = on;
+
+            // Perform compatibility checks for controls that might need
+            // to be disabled when on is set to true
+            if (on)
+            {
+                LateControlsCheck();
+            }
+        }
+
+        /// <summary>
+        /// Possibly disable some controls for components that are not compatible
+        /// with the installed version of Windows or system configuration
+        /// </summary>
+        private void LateControlsCheck()
+        {
+            step4HidHidePanel.IsEnabled = IsHidHideControlCompatible();
+            step5FakerInputPanel.IsEnabled = IsFakerInputControlCompatible();
         }
     }
 }

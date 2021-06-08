e6ae959cbc347c3140cd79f649ba95cf30714882
Travis Nickles <ryochan7@protonmail.com>
2021-06-07 21:40:17 -0500

    Delay log message for virtual device association
    Needed to ensure output slot index shown is correct

--- DS4Windows/DS4Control/ControlService.cs
+++ DS4Windows/DS4Control/ControlService.cs
@@ -999,7 +999,13 @@ namespace DS4Windows
                         success = true;
                     }
 
-                    if (success) LogDebug($"Associate X360 Controller in{(slotDevice.PermanentType != OutContType.None ? " permanent" : "")} slot #{slotDevice.Index+1} for input {device.DisplayName} controller #{index + 1}");
+                    if (success)
+                    {
+                        outputslotMan.EventDispatcher.BeginInvoke((Action)(() =>
+                        {
+                            LogDebug($"Associate X360 Controller in{(slotDevice.PermanentType != OutContType.None ? " permanent" : "")} slot #{slotDevice.Index + 1} for input {device.DisplayName} controller #{index + 1}");
+                        }));
+                    }
 
                     //tempXbox.Connect();
                     //LogDebug("X360 Controller #" + (index + 1) + " connected");
@@ -1069,7 +1075,13 @@ namespace DS4Windows
                         success = true;
                     }
 
-                    if (success) LogDebug($"Associate DS4 Controller in{(slotDevice.PermanentType != OutContType.None ? " permanent" : "")} slot #{slotDevice.Index + 1} for input {device.DisplayName} controller #{index + 1}");
+                    if (success)
+                    {
+                        outputslotMan.EventDispatcher.BeginInvoke((Action)(() =>
+                        {
+                            LogDebug($"Associate DS4 Controller in{(slotDevice.PermanentType != OutContType.None ? " permanent" : "")} slot #{slotDevice.Index + 1} for input {device.DisplayName} controller #{index + 1}");
+                        }));
+                    }
 
                     //DS4OutDevice tempDS4 = new DS4OutDevice(vigemTestClient);
                     //DS4OutDevice tempDS4 = outputslotMan.AllocateController(OutContType.DS4, vigemTestClient)

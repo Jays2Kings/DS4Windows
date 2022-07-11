acc7c2a10d8a059dec4d490064790eaa4273a005
Travis Nickles <ryochan7@protonmail.com>
2022-07-10 19:44:11 -0500

    Migrated project to use H.NotifyIcon.Wpf package
    Older Hardcodet.NotifyIcon.Wpf.NetCore project
    was deprecated

--- DS4Windows/DS4Forms/MainWindow.xaml.cs
+++ DS4Windows/DS4Forms/MainWindow.xaml.cs
@@ -373,9 +373,9 @@ namespace DS4WinWPF.DS4Forms
                 if (!IsActive && (Global.Notifications == 2 ||
                     (Global.Notifications == 1 && e.Warning)))
                 {
-                    notifyIcon.ShowBalloonTip(TrayIconViewModel.ballonTitle,
-                    e.Data, !e.Warning ? Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info :
-                    Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Warning);
+                    notifyIcon.ShowNotification(TrayIconViewModel.ballonTitle,
+                        e.Data, !e.Warning ? H.NotifyIcon.Core.NotificationIcon.Info :
+                        H.NotifyIcon.Core.NotificationIcon.Warning);
                 }
             }));
         }
@@ -638,8 +638,8 @@ Suspend support not enabled.", true);
         {
             if (!IsActive && (Global.Notifications == 2))
             {
-                notifyIcon.ShowBalloonTip(TrayIconViewModel.ballonTitle,
-                message, Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info);
+                notifyIcon.ShowNotification(TrayIconViewModel.ballonTitle,
+                    message, H.NotifyIcon.Core.NotificationIcon.Info);
             }
         }
 

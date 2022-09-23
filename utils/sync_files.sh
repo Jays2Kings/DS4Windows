0a2aef584a42295871341412210d5220ea0a5ab0
Travis Nickles <ryochan7@protonmail.com>
2022-09-23 16:06:06 -0500

    Attempt to use exclude-from arg for rsync

--- utils/sync_files.sh
+++ utils/sync_files.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
+# Check for presence of exclude patterns file for rsync with
+# EXCLUDE_PATTERNS_FILE envvar
+EXCLUDE_FROM_ARG=""
+if [ ! -z "${EXCLUDE_PATTERNS_FILE}" ]; then
+  EXCLUDE_FROM_ARG="--exclude-from=${EXCLUDE_PATTERNS_FILE}"
+fi
+
+echo "${EXCLUDE_FROM_ARG}"
+
 SOURCE_DIR=${1:-""}
 if [ ! -d "$SOURCE_DIR" ]; then
-  echo "Must specify an source directory"
+  echo "Must specify a source directory"
   exit 1
 fi
 
 DEST_DIR=${2:-""}
 if [ ! -d "$DEST_DIR" ]; then
-  echo "Must specify an destination directory"
+  echo "Must specify a destination directory"
   exit 1
 fi
 
-rsync -aP --exclude="ar" --exclude="cs" --exclude="de" --exclude="es" --exclude="fr" --exclude="he" \
-  --exclude="hu-HU" --exclude="it" --exclude="ja" --exclude="ko" --exclude="nl" --exclude="pl" \
-  --exclude="pt" --exclude="pt-BR" --exclude="ru" --exclude="se" --exclude="tr" --exclude="uk-UA" \
-  --exclude="zh-CN" --exclude="zh-Hans" --exclude="zh-Hant" --exclude="zh-TW" --exclude="Logs" \
-  --exclude="Profiles" --exclude="*.xml" --exclude="custom_exe_name.txt" \
-  --exclude="task.bat" --exclude="*.un~" "${SOURCE_DIR}" "${DEST_DIR}"
+rsync -aP "${EXCLUDE_FROM_ARG}" "${SOURCE_DIR}" "${DEST_DIR}"

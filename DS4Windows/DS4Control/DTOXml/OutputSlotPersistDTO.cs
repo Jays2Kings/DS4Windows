b7d44452ebecb2e5e4f376a674543eaae21dde21
Travis Nickles <ryochan7@protonmail.com>
2022-09-24 12:44:57 -0500

    Added DTO for OutputSlotManager

---
+++ DS4Windows/DS4Control/DTOXml/OutputSlotPersistDTO.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+using System.Xml;
+using System.Xml.Serialization;
+using DS4Windows;
+using NLog;
+
+namespace DS4WinWPF.DS4Control.DTOXml
+{
+    [XmlRoot("OutputSlots")]
+    public class OutputSlotPersistDTO : IDTO<OutputSlotManager>
+    {
+        [XmlAttribute("app_version")]
+        public string AppVersion
+        {
+            get => Global.exeversion;
+            set { }
+        }
+
+        [XmlElement("Slot")] // Use XmlElement here to skip container element
+        public List<OutputSlotSerializer> SlotItems
+        {
+            get; set;
+        }
+
+        public OutputSlotPersistDTO()
+        {
+            SlotItems = new List<OutputSlotSerializer>();
+        }
+
+        public void MapFrom(OutputSlotManager source)
+        {
+            foreach (OutSlotDevice dev in source.OutputSlots)
+            {
+                if (dev.CurrentReserveStatus == OutSlotDevice.ReserveStatus.Permanent)
+                {
+                    OutputSlotSerializer tempSlot = new OutputSlotSerializer()
+                    {
+                        Index = dev.Index,
+                        DeviceType = dev.PermanentType,
+                    };
+
+                    SlotItems.Add(tempSlot);
+                }
+            }
+        }
+
+        public void MapTo(OutputSlotManager destination)
+        {
+            foreach(OutputSlotSerializer tempSlot in SlotItems)
+            {
+                OutSlotDevice tempDev = null;
+                if (tempSlot.Index >= 0 && tempSlot.Index <= 3)
+                {
+                    int idx = tempSlot.Index;
+                    tempDev = destination.OutputSlots[idx];
+                }
+
+                if (tempDev != null)
+                {
+                    tempDev.PermanentType = tempSlot.DeviceType;
+                }
+            }
+        }
+    }
+
+    public class OutputSlotSerializer
+    {
+        [XmlAttribute("idx")]
+        public int Index
+        {
+            get; set;
+        } = 0;
+
+        [XmlElement("DeviceType")]
+        public OutContType DeviceType
+        {
+            get; set;
+        }
+    }
+}
--- DS4Windows/DS4Control/OutputSlotPersist.cs
+++ DS4Windows/DS4Control/OutputSlotPersist.cs
@@ -1,11 +1,13 @@
-﻿using DS4Windows;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Serialization;
+using DS4Windows;
+using DS4WinWPF.DS4Control.DTOXml;
 
 namespace DS4WinWPF.DS4Control
 {
@@ -16,39 +18,57 @@ namespace DS4WinWPF.DS4Control
         public static bool ReadConfig(OutputSlotManager slotManager)
         {
             bool result = false;
+
             string output_path = Path.Combine(Global.appdatapath, CONFIG_FILENAME);
             if (File.Exists(output_path))
             {
-                XmlDocument m_Xdoc = new XmlDocument();
-                try { m_Xdoc.Load(output_path); }
-                catch (UnauthorizedAccessException) { }
-                catch (XmlException) { }
-
-                XmlElement rootElement = m_Xdoc.DocumentElement;
-                if (rootElement == null) return false;
-
-                foreach(XmlElement element in rootElement.GetElementsByTagName("Slot"))
+                XmlSerializer serializer = new XmlSerializer(typeof(OutputSlotPersistDTO));
+                using StreamReader sr = new StreamReader(output_path);
+                try
                 {
-                    OutSlotDevice tempDev = null;
-                    string temp = element.GetAttribute("idx");
-                    if (int.TryParse(temp, out int idx) && idx >= 0 && idx <= 3)
-                    {
-                        tempDev = slotManager.OutputSlots[idx];
-                    }
-
-                    if (tempDev != null)
-                    {
-                        tempDev.CurrentReserveStatus = OutSlotDevice.ReserveStatus.Permanent;
-                        XmlNode tempNode = element.SelectSingleNode("DeviceType");
-                        if (tempNode != null && Enum.TryParse(tempNode.InnerText, out OutContType tempType))
-                        {
-                            tempDev.PermanentType = tempType;
-                        }
-                    }
+                    OutputSlotPersistDTO dto = serializer.Deserialize(sr) as OutputSlotPersistDTO;
+                    dto.MapTo(slotManager);
+                    result = true;
                 }
-
-                result = true;
+                catch (InvalidOperationException)
+                {
+                    result = false;
+                }
             }
+
+            //string output_path = Path.Combine(Global.appdatapath, CONFIG_FILENAME);
+            //if (File.Exists(output_path))
+            //{
+            //    XmlDocument m_Xdoc = new XmlDocument();
+            //    try { m_Xdoc.Load(output_path); }
+            //    catch (UnauthorizedAccessException) { }
+            //    catch (XmlException) { }
+
+            //    XmlElement rootElement = m_Xdoc.DocumentElement;
+            //    if (rootElement == null) return false;
+
+            //    foreach(XmlElement element in rootElement.GetElementsByTagName("Slot"))
+            //    {
+            //        OutSlotDevice tempDev = null;
+            //        string temp = element.GetAttribute("idx");
+            //        if (int.TryParse(temp, out int idx) && idx >= 0 && idx <= 3)
+            //        {
+            //            tempDev = slotManager.OutputSlots[idx];
+            //        }
+
+            //        if (tempDev != null)
+            //        {
+            //            tempDev.CurrentReserveStatus = OutSlotDevice.ReserveStatus.Permanent;
+            //            XmlNode tempNode = element.SelectSingleNode("DeviceType");
+            //            if (tempNode != null && Enum.TryParse(tempNode.InnerText, out OutContType tempType))
+            //            {
+            //                tempDev.PermanentType = tempType;
+            //            }
+            //        }
+            //    }
+
+            //    result = true;
+            //}
 
             return result;
         }
@@ -56,46 +76,93 @@ namespace DS4WinWPF.DS4Control
         public static bool WriteConfig(OutputSlotManager slotManager)
         {
             bool result = false;
-            XmlDocument m_Xdoc = new XmlDocument();
-            XmlNode rootNode;
-            rootNode = m_Xdoc.CreateXmlDeclaration("1.0", "utf-8", string.Empty);
-            m_Xdoc.AppendChild(rootNode);
-
-            rootNode = m_Xdoc.CreateComment(string.Format(" Made with DS4Windows version {0} ", Global.exeversion));
-            m_Xdoc.AppendChild(rootNode);
-
-            rootNode = m_Xdoc.CreateWhitespace("\r\n");
-            m_Xdoc.AppendChild(rootNode);
-
-            XmlElement baseElement = m_Xdoc.CreateElement("OutputSlots", null);
-            baseElement.SetAttribute("app_version", Global.exeversion);
-
-            int idx = 0;
-            foreach (OutSlotDevice dev in slotManager.OutputSlots)
-            {
-                if (dev.CurrentReserveStatus == OutSlotDevice.ReserveStatus.Permanent)
-                {
-                    XmlElement slotElement = m_Xdoc.CreateElement("Slot");
-                    slotElement.SetAttribute("idx", idx.ToString());
-
-                    XmlElement propElement;
-                    propElement = m_Xdoc.CreateElement("DeviceType");
-                    propElement.InnerText = dev.PermanentType.ToString();
-                    slotElement.AppendChild(propElement);
-
-                    baseElement.AppendChild(slotElement);
-                }
-
-                idx++;
-            }
-
-            m_Xdoc.AppendChild(baseElement);
 
             string output_path = Path.Combine(Global.appdatapath, CONFIG_FILENAME);
-            try { m_Xdoc.Save(output_path); result = true; }
-            catch (UnauthorizedAccessException) { result = false; }
+            string testStr = string.Empty;
+            XmlSerializer serializer = new XmlSerializer(typeof(OutputSlotPersistDTO));
+            using (StringWriter strWriter = new StringWriter())
+            {
+                using XmlWriter xmlWriter = XmlWriter.Create(strWriter,
+                    new XmlWriterSettings()
+                    {
+                        Encoding = Encoding.UTF8,
+                        Indent = true,
+                    });
+
+                // Write header explicitly
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteComment(string.Format(" Made with DS4Windows version {0} ", Global.exeversion));
+                xmlWriter.WriteWhitespace("\r\n");
+                xmlWriter.WriteWhitespace("\r\n");
+
+                // Write root element and children
+                OutputSlotPersistDTO dto = new OutputSlotPersistDTO();
+                dto.MapFrom(slotManager);
+                // Omit xmlns:xsi and xmlns:xsd from output
+                serializer.Serialize(xmlWriter, dto,
+                    new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty }));
+                xmlWriter.Flush();
+                xmlWriter.Close();
+
+                testStr = strWriter.ToString();
+                //Trace.WriteLine("TEST OUTPUT");
+                //Trace.WriteLine(testStr);
+            }
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(output_path, false))
+                {
+                    sw.Write(testStr);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                result = false;
+            }
 
             return result;
+
+            //XmlDocument m_Xdoc = new XmlDocument();
+            //XmlNode rootNode;
+            //rootNode = m_Xdoc.CreateXmlDeclaration("1.0", "utf-8", string.Empty);
+            //m_Xdoc.AppendChild(rootNode);
+
+            //rootNode = m_Xdoc.CreateComment(string.Format(" Made with DS4Windows version {0} ", Global.exeversion));
+            //m_Xdoc.AppendChild(rootNode);
+
+            //rootNode = m_Xdoc.CreateWhitespace("\r\n");
+            //m_Xdoc.AppendChild(rootNode);
+
+            //XmlElement baseElement = m_Xdoc.CreateElement("OutputSlots", null);
+            //baseElement.SetAttribute("app_version", Global.exeversion);
+
+            //int idx = 0;
+            //foreach (OutSlotDevice dev in slotManager.OutputSlots)
+            //{
+            //    if (dev.CurrentReserveStatus == OutSlotDevice.ReserveStatus.Permanent)
+            //    {
+            //        XmlElement slotElement = m_Xdoc.CreateElement("Slot");
+            //        slotElement.SetAttribute("idx", idx.ToString());
+
+            //        XmlElement propElement;
+            //        propElement = m_Xdoc.CreateElement("DeviceType");
+            //        propElement.InnerText = dev.PermanentType.ToString();
+            //        slotElement.AppendChild(propElement);
+
+            //        baseElement.AppendChild(slotElement);
+            //    }
+
+            //    idx++;
+            //}
+
+            //m_Xdoc.AppendChild(baseElement);
+
+            //string output_path = Path.Combine(Global.appdatapath, CONFIG_FILENAME);
+            //try { m_Xdoc.Save(output_path); result = true; }
+            //catch (UnauthorizedAccessException) { result = false; }
+
+            //return result;
         }
     }
 }

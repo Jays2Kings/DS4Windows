a29a96237dce7cc307cd10b49c3e19545f002af4
Travis Nickles <ryochan7@protonmail.com>
2023-09-10 13:50:09 -0500

    Changed props in HidDeviceCapabilities class to be full properties

--- DS4Windows/HidLibrary/HidDeviceCapabilities.cs
+++ DS4Windows/HidLibrary/HidDeviceCapabilities.cs
@@ -3,43 +3,58 @@
     public class HidDeviceCapabilities
     {
         private ushort usagePage;
+        private ushort usage;
+        private short inputReportByteLength;
+        private short outputReportByteLength;
+        private short featureReportByteLength;
+        private short[] reserved;
+        private short numberLinkCollectionNodes;
+        private short numberInputButtonCaps;
+        private short numberInputValueCaps;
+        private short numberInputDataIndices;
+        private short numberOutputButtonCaps;
+        private short numberOutputValueCaps;
+        private short numberOutputDataIndices;
+        private short numberFeatureButtonCaps;
+        private short numberFeatureValueCaps;
+        private short numberFeatureDataIndices;
 
         internal HidDeviceCapabilities(NativeMethods.HIDP_CAPS capabilities)
         {
-            Usage = capabilities.Usage;
+            usage = capabilities.Usage;
             usagePage = capabilities.UsagePage;
-            InputReportByteLength = capabilities.InputReportByteLength;
-            OutputReportByteLength = capabilities.OutputReportByteLength;
-            FeatureReportByteLength = capabilities.FeatureReportByteLength;
-            Reserved = capabilities.Reserved;
-            NumberLinkCollectionNodes = capabilities.NumberLinkCollectionNodes;
-            NumberInputButtonCaps = capabilities.NumberInputButtonCaps;
-            NumberInputValueCaps = capabilities.NumberInputValueCaps;
-            NumberInputDataIndices = capabilities.NumberInputDataIndices;
-            NumberOutputButtonCaps = capabilities.NumberOutputButtonCaps;
-            NumberOutputValueCaps = capabilities.NumberOutputValueCaps;
-            NumberOutputDataIndices = capabilities.NumberOutputDataIndices;
-            NumberFeatureButtonCaps = capabilities.NumberFeatureButtonCaps;
-            NumberFeatureValueCaps = capabilities.NumberFeatureValueCaps;
-            NumberFeatureDataIndices = capabilities.NumberFeatureDataIndices;
+            inputReportByteLength = capabilities.InputReportByteLength;
+            outputReportByteLength = capabilities.OutputReportByteLength;
+            featureReportByteLength = capabilities.FeatureReportByteLength;
+            reserved = capabilities.Reserved;
+            numberLinkCollectionNodes = capabilities.NumberLinkCollectionNodes;
+            numberInputButtonCaps = capabilities.NumberInputButtonCaps;
+            numberInputValueCaps = capabilities.NumberInputValueCaps;
+            numberInputDataIndices = capabilities.NumberInputDataIndices;
+            numberOutputButtonCaps = capabilities.NumberOutputButtonCaps;
+            numberOutputValueCaps = capabilities.NumberOutputValueCaps;
+            numberOutputDataIndices = capabilities.NumberOutputDataIndices;
+            numberFeatureButtonCaps = capabilities.NumberFeatureButtonCaps;
+            numberFeatureValueCaps = capabilities.NumberFeatureValueCaps;
+            numberFeatureDataIndices = capabilities.NumberFeatureDataIndices;
 
         }
 
-        public ushort Usage { get; private set; }
+        public ushort Usage { get => usage; private set => usage = value; }
         public ushort UsagePage { get => usagePage; private set => usagePage = value; }
-        public short InputReportByteLength { get; private set; }
-        public short OutputReportByteLength { get; private set; }
-        public short FeatureReportByteLength { get; private set; }
-        public short[] Reserved { get; private set; }
-        public short NumberLinkCollectionNodes { get; private set; }
-        public short NumberInputButtonCaps { get; private set; }
-        public short NumberInputValueCaps { get; private set; }
-        public short NumberInputDataIndices { get; private set; }
-        public short NumberOutputButtonCaps { get; private set; }
-        public short NumberOutputValueCaps { get; private set; }
-        public short NumberOutputDataIndices { get; private set; }
-        public short NumberFeatureButtonCaps { get; private set; }
-        public short NumberFeatureValueCaps { get; private set; }
-        public short NumberFeatureDataIndices { get; private set; }
+        public short InputReportByteLength { get => inputReportByteLength; private set => inputReportByteLength = value; }
+        public short OutputReportByteLength { get => outputReportByteLength; private set => outputReportByteLength = value; }
+        public short FeatureReportByteLength { get => featureReportByteLength; private set => featureReportByteLength = value; }
+        public short[] Reserved { get => reserved; private set => reserved = value; }
+        public short NumberLinkCollectionNodes { get => numberLinkCollectionNodes; private set => numberLinkCollectionNodes = value; }
+        public short NumberInputButtonCaps { get => numberInputButtonCaps; private set => numberInputButtonCaps = value; }
+        public short NumberInputValueCaps { get => numberInputValueCaps; private set => numberInputValueCaps = value; }
+        public short NumberInputDataIndices { get => numberInputDataIndices; private set => numberInputDataIndices = value; }
+        public short NumberOutputButtonCaps { get => numberOutputButtonCaps; private set => numberOutputButtonCaps = value; }
+        public short NumberOutputValueCaps { get => numberOutputValueCaps; private set => numberOutputValueCaps = value; }
+        public short NumberOutputDataIndices { get => numberOutputDataIndices; private set => numberOutputDataIndices = value; }
+        public short NumberFeatureButtonCaps { get => numberFeatureButtonCaps; private set => numberFeatureButtonCaps = value; }
+        public short NumberFeatureValueCaps { get => numberFeatureValueCaps; private set => numberFeatureValueCaps = value; }
+        public short NumberFeatureDataIndices { get => numberFeatureDataIndices; private set => numberFeatureDataIndices = value; }
     }
 }
